7f12f71bfb55a269e00309529f449a5793e87ae0
Amruth Kumar Balbail Yogendra <amruth518@gmail.com>
2025-08-04 01:23:17 +0100

    fix jenkins-3

--- global-setup.js
+++ global-setup.js
@@ -20,7 +20,7 @@ module.exports = async () => {
     // Fill in login credentials
     console.log('Filling login credentials...');
     await page.locator('#user-name').fill(process.env.LOGIN_USER);
-    await page.locator('#password').fill(process.env.PASSWORD);
+    await page.locator('#password').fill(process.env.LOGIN_PASSWORD);
     
     console.log('Clicking login button...');
     await page.locator('#login-button').click();
